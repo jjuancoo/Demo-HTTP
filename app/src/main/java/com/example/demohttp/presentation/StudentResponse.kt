571ae38b289133fd1266ef57130231d6b6e53949
Juanco <210373@utxicotepec.edu.mx>
2024-08-15 16:49:40 -0600

    Crea la dataclass de StudentResponse

---
+++ app/src/main/java/com/example/demohttp/presentation/StudentResponse.kt
@@ -0,0 +1,19 @@
+package com.example.demohttp.presentation
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serial
+
+data class StudentResponse(
+    @SerializedName("_id")
+    val _id: String,
+    @SerializedName("lastname")
+    val lastname: String,
+    @SerializedName("grade")
+    val grade: Int,
+    @SerializedName("group")
+    val group:String,
+    @SerializedName("career")
+    val career: String,
+    @SerializedName("average")
+    val average: Float
+)
