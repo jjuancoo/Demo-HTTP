8fc22d71fce83bbda174a50d6c7974fa3e7cdc48
Juanco <210373@utxicotepec.edu.mx>
2024-08-15 17:13:58 -0600

    Corrigiendo y creando la clase de Student

--- app/src/main/java/com/example/demohttp/presentation/StudentResponse.kt
+++ app/src/main/java/com/example/demohttp/presentation/StudentResponse.kt
@@ -3,16 +3,20 @@ package com.example.demohttp.presentation
 import com.google.gson.annotations.SerializedName
 
 data class StudentResponse(
-    @SerializedName("_id")
-    val _id: String,
-    @SerializedName("lastname")
-    val lastname: String,
-    @SerializedName("grade")
-    val grade: Int,
-    @SerializedName("group")
-    val group:String,
-    @SerializedName("career")
-    val career: String,
-    @SerializedName("average")
-    val average: Float
-)
+    val student: Student?
+){
+   data class Student(
+       @SerializedName("_id")
+       val _id: String,
+       @SerializedName("lastname")
+       val lastname: String,
+       @SerializedName("grade")
+       val grade: Int,
+       @SerializedName("group")
+       val group:String,
+       @SerializedName("career")
+       val career: String,
+       @SerializedName("average")
+       val average: Float
+   )
+}
